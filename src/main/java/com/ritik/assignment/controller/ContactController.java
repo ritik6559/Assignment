594ff9ee820c08e5e400c06834455374e3d3219e
ritik6559 <ritikjoshi741@gmail.com>
2025-02-14 00:24:40 +0530

    preparing for deployment

---
+++ src/main/java/com/ritik/assignment/controller/ContactController.java
@@ -0,0 +1,32 @@
+package com.ritik.assignment.controller;
+
+
+import com.ritik.assignment.request.IdentifyRequest;
+import com.ritik.assignment.response.ContactResponse;
+import com.ritik.assignment.services.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RestController
+@RequestMapping("${api.prefix}")
+@RequiredArgsConstructor
+public class ContactController {
+
+    private final ContactService contactService;
+
+    @PostMapping("/identify")
+    public ResponseEntity<ContactResponse> identify(@RequestBody IdentifyRequest request) {
+        try {
+            return ResponseEntity.ok(contactService.saveRequest(request));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+}
