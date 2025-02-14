9f1b5388b4836c5faf66cc6718c61a9547f77fd9
ritik6559 <ritikjoshi741@gmail.com>
2025-02-15 01:50:47 +0530

    fixing dns errors

--- src/main/java/com/ritik/assignment/controller/ContactController.java
+++ src/main/java/com/ritik/assignment/controller/ContactController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestController
-@RequestMapping("${api.prefix}")
+@RequestMapping
 @RequiredArgsConstructor
 public class ContactController {
 
